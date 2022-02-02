98d04fe0166b871a793ee6bf10ad8f57f951ac1d
Felipe <felmartins1985@gmail.com>
2022-02-02 13:47:25 -0300

    7 exercicio feito

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -58,7 +58,20 @@ switch (peca){
         console.log("Rainha-> diagonal, horizontal e vertical")
         break;
 }
-
+let nota=120;
+if(nota<0 || nota>100){
+    console.log("Erro");
+} else if(nota>=90){
+    console.log("A");
+} else if(nota>=80){
+    console.log("B");
+} else if(nota>=70){
+    console.log("C");
+} else if (nota>=60){
+    console.log("D");
+} else if(nota>=50){
+    console.log("E");
+}
 
   
 
