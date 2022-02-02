b0d61fb889d5a3c7a05ea7b56f206bb4734a3193
Felipe <felmartins1985@gmail.com>
2022-02-02 10:43:57 -0300

    fixar 3 e 4

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/fixar3.js
@@ -0,0 +1,10 @@
+const nota=50;
+if (nota>=80) {
+    console.log("Parabéns, você foi aprovado(a)!");
+    }
+else if (nota<80){
+    console.log("Você está na nossa lista de espera");
+    }
+else if (nota<60){
+    console.log("Você foi reprovado");
+}
