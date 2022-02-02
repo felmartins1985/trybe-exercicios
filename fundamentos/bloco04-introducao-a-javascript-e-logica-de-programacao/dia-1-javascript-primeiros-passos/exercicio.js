a307aefe59ab7e12c21ba2577192ce12c7dbd43e
Felipe <felmartins1985@gmail.com>
2022-02-02 13:32:38 -0300

    5 exercicio feito

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -31,4 +31,18 @@ const h=4;
     } else{
         console.log("zero");
     }
+let angulaA=50;
+let angulaB=60;
+let angulaC=70;
+let somaDosAngulas= angulaA+angulaB+angulaC;
+let todosOsAngulasSaoPositivos= angulaA>0 && angulaB>0 && angulaC>0;
+if(todosOsAngulasSaoPositivos){
+    if(somaDosAngulas=180){
+        console.log(true);
+    } else {
+        console.log(false)
+    }
+} else {
+    console.log("Erro");
+}
 
