421d9d88021b9191f3444523718be6b59e3e00f5
Felipe <felmartins1985@gmail.com>
2022-02-02 10:27:33 -0300

    para fixar 1 e 2

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/fixar1.js
@@ -0,0 +1,13 @@
+const myName = "Felipe Costa Martins";
+const birthCity= "Rio de Janeiro";
+let birthYear= 1985;
+console.log(myName);
+console.log(birthCity);
+console.log (birthYear);
+birthYear= 2030;
+console.log(birthYear);
+const birthCity= "BH";
+console.log(birthCity);
+
+
+
