6e12e27cb712bd13d4d0ef92c7580b26187653e4
Felipe <felmartins1985@gmail.com>
2022-02-02 13:17:21 -0300

    2 exercicio feito

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -1,7 +1,16 @@
 const a=10;
 const b=2;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+console.log("Soma " + (a+b));
+console.log("Subtração "+ (a-b));
+console.log("Multiplicação "+ (a*b));
+console.log("Divisão "+ (a/b));
+console.log("Módulo " + (a%b));
+
+const c=5;
+const d=7;
+if (c>d){
+    console.log("O número " +c+ " é o maior");
+} else if (d>c) {
+    console.log ("O número "+ d+" é o maior")
+}
+
