29e7900b3d5da97166e8a12c3d726a9e23a57789
Felipe <felmartins1985@gmail.com>
2022-02-03 09:17:29 -0300

    exercicios 5 e 6 feitos

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -23,3 +23,24 @@ if (media>20){
 } else {
     console.log("valor menor ou igual a 20");
 }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior= 1;
+for (let index=0;index<numbers.length;index+=1){
+    if(numbers[index]>maior){
+        maior=numbers[index];
+    }
+}console.log(maior);
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar=0;
+for (let index=0; index<numbers.length; index+=1){
+    if (numbers[index]%2 !==0){
+        impar+=1;
+    }
+} 
+if (impar===0){
+    console.log("nenhum valor encontrado");
+} else{
+    console.log(impar);
+}
