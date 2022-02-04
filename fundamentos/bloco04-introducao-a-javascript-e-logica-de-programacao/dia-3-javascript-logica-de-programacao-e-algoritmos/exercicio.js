c6e2ea373ac33e9706d527ed3b0d8c3513a53b68
Felipe <felmartins1985@gmail.com>
2022-02-04 09:04:55 -0300

    exercicio 3 feito

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -12,4 +12,20 @@ for (index=word.length-1;index>=0;index-=1){
     invert=word[index];
     console.log(invert);
 }
- 
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorArray= array[0];
+let menorArray= array[0];
+for (let index=0; index<array.length; index+=1){
+    if(array[index].length>maiorArray.length){
+        maiorArray= array[index];
+    }
+}
+console.log(maiorArray);
+
+for (let index2=0; index2<array.length;index2+=1){
+    if(array[index2].length<menorArray.length){
+        menorArray=array[index2];
+    }
+}
+console.log(menorArray);
