c583f496b49404a860c28700e51b0e1f1b70b8e6
Felipe <felmartins1985@gmail.com>
2022-02-04 08:43:20 -0300

    exercicio 1 e 2 feitos

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -0,0 +1,15 @@
+let fator=10;
+for (let index=10;index>0;index-=1){
+    fator*=index;
+}
+console.log(fator);
+
+let word="tryber";
+let invertido= word.split("").reverse().join("");
+let invert="";
+console.log(invertido);
+for (index=word.length-1;index>=0;index-=1){
+    invert=word[index];
+    console.log(invert);
+}
+ 
