83d6464b1215fa10db4fe3b53f6e78ed42d1a9b0
Felipe <felmartins1985@gmail.com>
2022-02-04 09:55:16 -0300

    exercicio 4 feito

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -29,3 +29,19 @@ for (let index2=0; index2<array.length;index2+=1){
     }
 }
 console.log(menorArray);
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
