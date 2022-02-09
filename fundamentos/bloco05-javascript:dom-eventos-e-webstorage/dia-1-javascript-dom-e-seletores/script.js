191952b803a5a3051f86385ad062bb6ea8b0f728
Felipe <felmartins1985@gmail.com>
2022-02-09 15:40:55 -0300

    atualizacao do para fixar

--- fundamentos/bloco05-javascript:dom-eventos-e-webstorage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco05-javascript:dom-eventos-e-webstorage/dia-1-javascript-dom-e-seletores/script.js
@@ -3,9 +3,8 @@ header.style.backgroundColor="red";
 const classs= document.getElementsByClassName("emergency-tasks")[0];
 classs.style.backgroundColor="black";
 const hh3= document.querySelectorAll(".no-emergency-tasks h3");
-for (let index=0;index<hh3.length;index+=1){
-    hh3[index].style.backgroundColor="blue";
-}
+hh3[1].style.backgroundColor="blue";
+
 const foot=document.getElementById("footer-container");
 foot.style.backgroundColor="yellow";
 
