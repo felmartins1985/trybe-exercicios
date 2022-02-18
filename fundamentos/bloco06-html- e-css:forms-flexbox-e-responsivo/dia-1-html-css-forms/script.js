4775268b74145c1e959836c5d2dabde4772578a9
Felipe <felmartins1985@gmail.com>
2022-02-18 19:25:20 -0300

    atualizacao dos comentarios

--- fundamentos/bloco06-html- e-css:forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco06-html- e-css:forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,3 +1,9 @@
+/* dentro da função de lidar com o envio 
+eu colocao a função invalidação do input
+e faço uma condicional que perguntara se a validação é falsa
+caso seja, emitira um alerta dizendo dados invalidos
+caso nao seja, emitira um alerta dizendo que foram enviados com sucesso
+ */
 function lidarEnvio(event) {
     event.preventDefault();
     const validacao = invalidacaoDoInput();
@@ -8,10 +14,22 @@ function lidarEnvio(event) {
     }
   }
 
+/* eu crio a função para liberar o envio somente se o usuario concordar em divulgar as fotos
+entao crio uma variavel para receber o botao Enviar
+e crio uma variavel para receber o input que faz concordar com a divulgacao
+ao final, eu peço para que a variavel criada para o botao inverta seu atributo
+qual seja, o disabled, caso o usuario tenha concordado com a divulgacao
+para isso ocorrer, eu utilizo o operador chamado "bang" 
+e ele representa uma negação, ou seja, o contrário do retorno de alguma coisa. 
+Se algo for verdadeiro ele retornará falso e vice-versa.
+Sendo assim, se a checkbox estiver "checkada" nosso botão não estará 
+desabilitado (estará habilitado). Ou seja, ela transforma o disabled que era verdadeiro em falso
+sendo possivel entao, enviar o formulario.
+
+ */
 function liberarEnvio(){
     let botaoSubmt=document.querySelector('#enviar');
     let divulgar=document.querySelector('#divulgacao');
-    console.log(divulgar);
     botaoSubmt.disabled=!divulgar.checked;
 }
 
@@ -23,7 +41,13 @@ window.onload=function(){
 }
 
 
-
+/* essa função é para invalir o input dos limites de caracteres
+eu crio 3 variaveis para receber a ids que tem limite de caracteres
+depois eu crio outras 3 variaves com o que vem fora do limite de caracteres
+apos isso, faço uma condicional dizendo se caso uma das variaveis esteja fora 
+do limite de caracteres será retornado false 
+caso contrario, sera retornado verdadeiro
+ */
 function invalidacaoDoInput() {
     const email = document.querySelector('#email-interessado').value.length;
     const invalidEmail = email < 10 || email > 50;
