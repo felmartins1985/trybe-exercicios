06a297fc8dbf6a9cddf9755064b39c9c5e02404c
Felipe <felmartins1985@gmail.com>
2022-03-02 17:10:01 -0300

    exercicios feitos

--- fundamentos/bloco07-introducao-a-javascript-es6-e-testesunitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco07-introducao-a-javascript-es6-e-testesunitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -14,6 +14,11 @@
 
   const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
+ oddsAndEvens.sort(function(a, b) {
+   return a - b;
+ });
+ console.log(oddsAndEvens);
+ 
   const sortOddsAndEvens=()=>{
     oddsAndEvens[0]=2;
     oddsAndEvens[1]=3;
@@ -22,7 +27,38 @@
     oddsAndEvens[4]=10;
     oddsAndEvens[5]=13;
     return oddsAndEvens;
-}
+    };
   const sortedArrays=sortOddsAndEvens();
 
   console.log(`os números ${sortedArrays} estão em ordem crescente!`);
+
+//parte 2
+//exercicio 1
+const factorial=(number)=>{
+    let result=1;
+    for (let i=2; i<=number; i++){
+        result=result*i;
+    }
+    return result;
+}
+console.log(factorial(5));
+
+// //exercicio 2
+// lembrar que o slipt vai transformar a string em arrays, contendo cada posição uma palavra
+const longestWord = text => {
+    let wordArray = text.split(' ');
+    let maxLength = 0;
+    let result = '';
+    console.log(wordArray);
+
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+            result = word;
+        }
+    }
+
+    return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
