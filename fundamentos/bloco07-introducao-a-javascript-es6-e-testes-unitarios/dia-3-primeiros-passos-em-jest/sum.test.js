252ed8c0869cad88464072a1aa5be5bf33275bbd
Felipe <felmartins1985@gmail.com>
2022-03-07 10:42:59 -0300

    exercicio 1 feito

---
+++ fundamentos/bloco07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,17 @@
+
+describe('testa a função sum', () => {
+  const { describe } = require('yargs');
+  const sum = require('./sum')
+  it('o retorno da soma dever ser 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  })
+  it('o retorno da soma deve ser zero', () => {
+    expect(sum(0, 0)).toBe(0);
+  })
+  it('a função deve retornar um erro', () => {
+    expect(() => sum(4, "5")).toThrow();
+  })
+  it('a função deve retornar um erro parameters must be numbers', () => {
+    expect(() => sum(4, "5")).toThrowError(new Error(('parameters must be numbers')));
+  })
+})
