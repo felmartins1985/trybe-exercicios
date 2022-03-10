f6b03913ef02c3a010d55866ed80936ef2d94258
Felipe <felmartins1985@gmail.com>
2022-03-10 06:58:01 -0300

    atualizacoes

--- fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,13 +1,14 @@
-const employeeGenerator = (nome, age) => {
+const employeeGenerator = (nome) => {
   const email = nome.toLowerCase().split(' ').join('_');
-  return { nome, email: `${email}@trybe.com`, age };
+  return { nome, email: `${email}@trybe.com`};
 };
 console.log(employeeGenerator('felipe', 36));
+
 const newEmployees = (callback) => {
   const employees = {
-    id1: callback('Pedro Guerra'),
-    id2: callback('Luiza Drumond'),
-    id3: callback('Carla Paiva'),
+    id1: callback('Pedro Guerra'), //employeeGenerator()
+    id2: callback('Luiza Drumond'),//employeeGenerator()
+    id3: callback('Carla Paiva'), //employeeGenerator()
   };
   return employees;
 };
@@ -23,8 +24,8 @@ const checkNumbers = (number1, number2) => {
 }
 
 const loteryResult = (number1, callback) => {
-  const number2= Math.floor((Math.random()*5)+1);
-  return callback(number1,number2);
+  const number2 = Math.floor((Math.random() * 5) + 1);
+  return callback(number1, number2);//callback=checkNumbers(number1, number2);
 }
 console.log(loteryResult(2, checkNumbers));
 // 3
@@ -44,9 +45,16 @@ const countPoints = (rightAnswers, studentAnswers, callback) => {
   let contador = 0;
   for (let index = 0; index < rightAnswers.length; index += 1) {
     const actionReturn = callback(rightAnswers[index], studentAnswers[index]);
+    //compareAnswers((rightAnswers[index], studentAnswers[index]))
     contador += actionReturn;
   }
   return `Resultado final: ${contador} pontos`;
 };
 
 console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+// const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+// const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+// for (let index = 0; index < rightAnswers.length; index += 1) {
+//   const actionReturn = callback(rightAnswers[index], studentAnswers[index]);
+//   contador += actionReturn;
+// }
