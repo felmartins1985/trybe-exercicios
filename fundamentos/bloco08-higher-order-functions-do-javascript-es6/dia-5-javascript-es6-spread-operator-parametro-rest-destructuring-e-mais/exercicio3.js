9e45b747111d2dbd4c18cf8cb6f77a127ef57369
Felipe <felmartins1985@gmail.com>
2022-03-15 15:05:05 -0300

    exercicio 4

---
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio3.js
@@ -0,0 +1,22 @@
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+//ao clocar dentro do parametro as chaves que desejo que sejam impressas
+//ao dar console.log com o nome da variavel do objeto, a função irá buscar dentro desse objeto
+//as chaves em questao para retornar seus valores
+const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
