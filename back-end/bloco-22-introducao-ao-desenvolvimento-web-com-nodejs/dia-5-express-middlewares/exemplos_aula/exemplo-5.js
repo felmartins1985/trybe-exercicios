a35d2d81f8244edf59095b0bb995d7a6fc91e733
Felipe <felmartins1985@gmail.com>
2022-08-04 08:01:05 -0300

    refactor: refatorando os exercicios

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exemplos_aula/exemplo-5.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exemplos_aula/exemplo-5.js
@@ -21,11 +21,3 @@ app.listen(3002);
 
 //
 /* errorMiddleware.js */
-
-module.exports = (err, req, res, next) => {
-  if (err.code && err.status) {
-    return res.status(err.status).json({ message: err.message, code: err.code });
-  }
-
-  return res.status(500).json({ message: err.message });
-}
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exemplos_aula/index-1.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exemplos_aula/index-1.js
@@ -16,7 +16,11 @@ const recipesRouter = require('./recipesRouter');
 // Repare no uso de mais um parâmetro na chamada à função app.use. 
 //Isso diz ao Express que queremos que aquele middleware (no caso o router)
 // seja executado para qualquer rota que comece com aquele caminho.
+//Repare que, ao registrar uma rota no router, não precisamos repetir a parte do caminho 
+//que já passamos para app.use. É por isso que a rota que definimos com router.get('/:id') 
+//na verdade se torna acessível através de /recipes/:id.
 app.use('/recipes', recipesRouter);
+// e como se eu dissesse que o '/' definido ao criaro recipesRouter é o '/recipes'
 
 // app.all('*', function (req, res) {
 // 	return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
