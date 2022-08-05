6e2b82eee5f5c2c3030ba1cc7d42138a70c976ec
Felipe <felmartins1985@gmail.com>
2022-08-05 14:43:33 -0300

    refactor: refatorando o codigo adicionando respostas dadas pelo curso

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/index.js
@@ -8,16 +8,17 @@ const rescue = require('express-rescue');
 const PORT = 3000;
 //
 const createUser= require('./middlewares/createUser');
+const updatedUser = require('./middlewares/updateUser');
 // 1
 app.post('/user',createUser.verifyFirstName,createUser.verifyLastName,
 createUser.verifyEmail, createUser.verifyPassword,
 rescue (async (req,res)=>{
   const{firstName,lastName,email,password}=req.body;
-  if(!firstName || !lastName || !email || !password){
-    return res.status(400).json({message: 'Dados inválidos'});
-  }
+  // if(!firstName || !lastName || !email || !password){
+  //   return res.status(400).json({message: 'Dados inválidos'});
+  // }
   const newUser= await User.create({firstName,lastName,email,password});
-  return res.status(200).json({newUser});
+  return res.status(200).json(newUser);
 }))
 // 2
 app.get('/user', rescue(async (req, res) => {
@@ -34,7 +35,10 @@ app.get('/user/:id', rescue(async (req, res) => {
   return res.status(200).json({user});
 }))
 // 4
-app.put('/user/:id', rescue((req, res) => {
+app.put('/user/:id',updatedUser.verifyFirstName,updatedUser.verifyLastName,
+updatedUser.verifyEmail,
+updatedUser.verifyPassword,
+rescue(async (req, res) => {
   const {id}=req.params;
   const {firstName,lastName,email,password}=req.body;
   const updateUser= await User.updateUser(id,{firstName,lastName,email,password});
@@ -43,12 +47,5 @@ app.put('/user/:id', rescue((req, res) => {
   }
   return res.status(200).json({updateUser});
 }))
-
-
-
-
-
-
-
 // Iniciamos o servidor
 app.listen(PORT, () => { console.log(`App listening on port ${PORT}`); });
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/models/User.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/models/User.js
@@ -7,18 +7,25 @@ const serialize = (userData) => ({
 
   const create = async ({firstName,lastName,email, password}) => 
   {
-    const user= connection.execute('INSERT INTO users_crud.users (first_name,last_name,email,password) VALUES (?,?,?,?)',
+    const user= await connection.execute('INSERT INTO users_crud.users (first_name,last_name,email,password) VALUES (?,?,?,?)',
     [firstName, lastName, email, password]);
     const newUser={id: user.insertId, firstName, lastName, email};
     return newUser;
   };
-
+//
   const findAll= async () => {
     const users = await connection.execute(
       'SELECT * FROM users_crud.users;',
     );
       return users.map(serialize);
   };
+  // function findAll() {
+  //   // Mais uma vez, chamamos connection.execute para executar nossa query.
+  //   return connection.execute('SELECT * from users;')
+  //     // Passamos cada resultado pela função de formatação
+  //     .then(([results]) => results.map(formatUser));
+  // }
+  //
   const findById = async (id) => {
     const query = 'SELECT * FROM users_crud.users WHERE id = ?'
     const user = await connection.execute(query, [id]);
@@ -27,6 +34,19 @@ const serialize = (userData) => ({
     const findUser = user.map(serialize)[0];
     return findUser;
   };
+  // function findById(id) {
+  //   // Realizamos uma consulta buscando o usuário por ID
+  //   return connection.execute('SELECT * FROM users WHERE id = ?', [id])
+  //     .then(([results]) => {
+  //     // caso encontre o usuário pelo id, ele será o 1º item no result.
+  //     // assim podemos formatá-lo e retornar os dados
+  //     if (results[0]) return formatUser(results[0]);
+  //     // caso contrário retorne nulo
+  //     return null;
+  //     });
+  // }
+  
+  //
   const updateUser= async(id, {firstName,lastName,email, password}) => {
     await connection.execute(
       'UPDATE users_crud.users SET first_name = ?, last_name = ?, email = ?, password = ? WHERE id = ?',
@@ -34,9 +54,21 @@ const serialize = (userData) => ({
     );
     return findById(id);
   }
+  // const Joi = require('joi');
 
+  // const userSchema = Joi.object({
+  //   firstName: Joi.string().required(),
+  //   lastName: Joi.string().required(),
+  //   email: Joi.string().email().required(),
+  //   password: Joi.string().min(6).required()})
+  
+  // // Criamos um método para verificar se os dados do usuário são válidos
+  // function isValid(userData) {
+  //   return userSchema.validate(userData);
+  // }
+
+//
   module.exports={
-    serialize,
     create,
     findAll,
     findById,
