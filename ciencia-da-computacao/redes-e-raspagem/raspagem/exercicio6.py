6fa05562cc5689cf547364773e751851ef1d4146
hp <hp@pop-os.localdomain>
2023-01-13 15:10:09 -0300

    feat: exercicios

--- ciencia-da-computacao/redes-e-raspagem/raspagem/exercicio6.py
+++ ciencia-da-computacao/redes-e-raspagem/raspagem/exercicio6.py
@@ -4,5 +4,5 @@ from pymongo import MongoClient
 category = input("Escolha uma categoria: ")
 with MongoClient() as client:
     db = client.library
-    for book in db.books.find({"categories": 'Java'}, projection=["title"]):
+    for book in db.books.find({"categories": client}, projection=["title"]):
         print(book["title"])
