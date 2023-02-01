c289128db146dcc2c358e59145622a37d8c07799
hp <hp@pop-os.localdomain>
2023-02-01 15:38:49 -0300

    feat: exercicio 6

---
+++ ciencia-da-computacao/estrutura-de-dados/dia04/exercicio6.py
@@ -0,0 +1,32 @@
+from stack import Stack
+
+
+def solve_expression(expr):
+    stack = Stack()
+    # print(expr)
+    tokens_list = expr.split(" ")
+    # print(tokens_list)
+    for token in tokens_list:
+        if token == "+":
+            # Sum operation
+            result = stack.pop() + stack.pop()
+            stack.push(result)
+        elif token == "*":
+            # multiply operation
+            result = stack.pop() * stack.pop()
+            stack.push(result)
+        elif token == "-":
+            # subtract operation
+            result = stack.pop() - stack.pop()
+            stack.push(result)
+        elif token == "/":
+            # divide operation
+            result = stack.pop() / stack.pop()
+            stack.push(result)
+        else:
+            # add number operation
+            stack.push(int(token))
+    return stack.pop()
+
+
+print(solve_expression("5 10 + 3 *"))
